6488e1b45a3dc2ecfd0cdd3e5b8b0bc5a0f870f3
Kalle Chakradhar <kallec@wugroup.co>
2026-08-31 11:54:07 +0800

    Disable strict SSL verification to fix Docker CA cert issues

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://NxhLTE2TjPVqirD.root:7PkdDwgmEcQQUuRW@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/test?ssl_verify_cert=true&ssl_verify_identity=true"
+DATABASE_URL = "mysql+pymysql://NxhLTE2TjPVqirD.root:7PkdDwgmEcQQUuRW@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/test"
 
 # SQLAlchemy requires 'postgresql://' instead of 'postgres://'
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# check_same_thread is only needed for SQLite
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+# check_same_thread is only needed for SQLite, and we need ssl dict for TiDB to bypass strict CA checks in Docker
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {"ssl": {"ssl_verify_cert": False, "ssl_verify_identity": False}}
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
